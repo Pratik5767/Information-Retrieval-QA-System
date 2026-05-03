fc8521b67bf54b895019ee524495785a3f265093
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-05-03 21:12:23 +0530

    execute the setup.py and following files generated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="QApplication",
+    version='0.0.1',
+    author="Pratik Salunkhe",
+    author_email="pratikvsalunkhe924@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
